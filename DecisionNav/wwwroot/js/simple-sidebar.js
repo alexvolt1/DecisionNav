639afc55751b4ef5609488ac1c2fc6300fbb0d07
alexV <alexV@DESKTOP-UKJJB1E>
2019-04-23 01:13:23 -0400

    Add project files.

---
+++ DecisionNav/wwwroot/js/simple-sidebar.js
@@ -0,0 +1,38 @@
+﻿(function ($) {
+    "use strict"; // Start of use strict
+
+    // Toggle the side navigation
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $('.nav-link-collapse').on('click', function () {
+        $('.nav-link-collapse').not(this).removeClass('nav-link-show');
+        $(this).toggleClass('nav-link-show');
+    });
+
+
+
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
+
+        return false;
+    });
+
+
+
+
+
+
+
+
+})(jQuery); // End of use strict
