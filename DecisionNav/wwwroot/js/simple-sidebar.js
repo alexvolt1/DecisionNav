bdd263d0a537651a688a6a91709fed07a25aa007
alex volt <alexvolt@hotmail.com>
2019-04-24 17:01:11 -0400

    comm

--- DecisionNav/wwwroot/js/simple-sidebar.js
+++ DecisionNav/wwwroot/js/simple-sidebar.js
@@ -39,7 +39,15 @@
 
 
 
-
+    $(".btn-group, .dropdown").hover(
+        function () {
+            $('>.dropdown-menu', this).stop(true, true).fadeIn("fast");
+            $(this).addClass('open');
+        },
+        function () {
+            $('>.dropdown-menu', this).stop(true, true).fadeOut("fast");
+            $(this).removeClass('open');
+        });
 
 
 
